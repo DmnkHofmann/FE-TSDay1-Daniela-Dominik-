74e81745303e8ce98ab5f2c70ee6dd892b80d24e
ajnacodes <b.daniela196@outlook.com>
2022-06-08 14:19:49 +0200

    first push

---
+++ Daniela/script.ts
@@ -0,0 +1,81 @@
+// basic exercise one
+
+// function multi(max: number = 10){
+//     for(let i = 1; i <= max; i++){
+//         for(let j = 1; j <= max; j++){
+//             let result: number = i * j;
+//             let resultString: string = `${j} x ${i} = ${result} <br/>`;
+//             document.write(resultString);
+//         }
+//     }
+// }
+// multi();
+
+//  or like this
+// let arrayOFNumbers : number[]= [1,2,3,4,5,6,7,8,9,10];
+// for(let i: number = 1; i <= 10; i++){
+//     for(let j: number = 1; j <= 10; j++){
+//         console.log(i + "x" + j + "=" + i*j)
+//     }
+// }
+
+// ================================================================================================
+
+
+
+
+// basic exericse two
+// let naamess = [
+//     {fName: 'Daniela',
+//     lName: 'Buga'},
+//     {fName: 'Daniela',
+//     lName: 'Buga'},
+//     {fName: 'Daniela',
+//     lName: 'Buga'},
+//     {fName: 'Daniela',
+//     lName: 'Buga'},
+//     {fName: 'Daniela',
+//     lName: 'Buga'},
+//     {fName: 'Daniela',
+//     lName: 'Buga'},
+//     {fName: 'Daniela',
+//     lName: 'Buga'},
+//     {fName: 'Daniela',
+//     lName: 'Buga'},
+//     {fName: 'Daniela',
+//     lName: 'Buga'}
+// ];
+// naamess.forEach((naame) => {
+//     document.write("I am" + naame.fName)
+// })
+
+
+// let fullName: {fName:string, lName: string};
+
+// fullName = {
+//     fName: 'Daniela',
+//     lName: 'Buga'
+// };
+
+// console.log(fullName);
+
+// for (let i = 0; i < 10; i++) {
+//     document.write(`The full name is: ${fullName.fName} ${fullName.lName} <br/> `);
+
+//     setTimeout (function(){
+//         console.log(`The full name is: ${fullName.fName} ${fullName.lName}`);
+//     },5000)
+   
+// }
+
+
+// basic exercise three
+
+// let myArray: string[] = ["Jinx", "Heimerdinger", "Aphelios", "Morgana", "Soraka"];
+
+// for (let i = 0; i < myArray.length; i++) {
+//     console.log(`Array element: ${myArray[i]}`);
+//     console.log(`Array element index: ${i}`);
+//     console.log(`Array element: ${myArray[i]} with index ${i}`);
+// }
+
